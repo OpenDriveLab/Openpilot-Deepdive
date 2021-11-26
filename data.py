2eee1fbaf7f39fc7248444b299db7ba068bd94e6
tangtutian.vendor <tangtutian.vendor@senseauto.com>
2021-11-26 20:36:10 +0800

    update comma2k19 dataset

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import os
+import io
 import json
 import torch
+from math import pi
 import numpy as np
 import cv2
 cv2.setNumThreads(0)
@@ -11,6 +13,9 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from utils import warp, generate_random_params_for_warp
 from preprocess_view_transform import calibration
+
+import utils_comma2k19.orientation as orient
+import utils_comma2k19.coordinates as coord
 
 
 class PlanningDataset(Dataset):
@@ -35,7 +40,7 @@ class PlanningDataset(Dataset):
         self.enable_aug = False
         self.view_transform = False
 
-        self.use_memcache = True
+        self.use_memcache = False
         if self.use_memcache:
             self._init_mc_()
 
@@ -148,13 +153,121 @@ class SequencePlanningDataset(PlanningDataset):
         )
 
 
+class Comma2k19SequenceDataset(PlanningDataset):
+    def __init__(self, split_txt_path, prefix, num_pts=10, use_memcache=True):
+        self.split_txt_path = split_txt_path
+        self.prefix = prefix
+
+        self.samples = open(split_txt_path).readlines()
+        self.samples = [i.strip() for i in self.samples]
+
+        self.num_pts = num_pts
+        self.fix_seq_length = 40
+
+        self.transforms = transforms.Compose(
+            [
+                # transforms.Resize((900 // 2, 1600 // 2)),
+                # transforms.Resize((9 * 32, 16 * 32)),
+                transforms.Resize((128, 256)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.3890, 0.3937, 0.3851],
+                                     [0.2172, 0.2141, 0.2209]),
+            ]
+        )
+
+        self.use_memcache = use_memcache
+        if self.use_memcache:
+            self._init_mc_()
+
+    def _get_cv2_vid(self, path):
+        if self.use_memcache:
+            path = self.client.get(str(path), client_method='get_object', expires_in=3600)
+        return cv2.VideoCapture(path)
+
+    def _get_numpy(self, path):
+        if self.use_memcache:
+            bytes = io.BytesIO(memoryview(self.client.get(str(path))))
+            return np.lib.format.read_array(bytes)
+        else:
+            return np.load(path)
+
+    def __getitem__(self, idx):
+        seq_sample_path = self.prefix + self.samples[idx]
+        cap = self._get_cv2_vid(seq_sample_path + '/video.hevc')
+        if (cap.isOpened() == False):
+            raise RuntimeError
+        imgs = []  # <--- all frames here
+        while (cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                imgs.append(frame)
+            else:
+                break
+        cap.release()
+
+        seq_length = len(imgs)
+
+        if seq_length < self.fix_seq_length + self.num_pts:
+            raise RuntimeError('The length of sequence', seq_sample_path, 'is too short',
+                               '(%d < %d)' % (seq_length, self.fix_seq_length + self.num_pts))
+        
+        seq_length_delta = seq_length - (self.fix_seq_length + self.num_pts)
+        seq_length_delta = np.random.randint(1, seq_length_delta+1)
+
+        seq_start_idx = seq_length_delta
+        seq_end_idx = seq_length_delta + self.fix_seq_length
+
+        # seq_input_img
+        imgs = imgs[seq_start_idx-1: seq_end_idx]  # contains one more img
+        imgs = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in imgs]
+        imgs = list(Image.fromarray(img) for img in imgs)
+        imgs = list(self.transforms(img)[None] for img in imgs)
+        input_img = torch.cat(imgs, dim=0)  # [N+1, 3, H, W]
+        input_img = torch.cat((input_img[:-1, ...], input_img[1:, ...]), dim=1)
+
+        # poses
+        frame_positions = self._get_numpy(self.prefix + self.samples[idx] + '/global_pose/frame_positions')[seq_start_idx: seq_end_idx+self.num_pts]
+        frame_orientations = self._get_numpy(self.prefix + self.samples[idx] + '/global_pose/frame_orientations')[seq_start_idx: seq_end_idx+self.num_pts]
+
+        # TODO: Time-Anchor like OpenPilot
+        future_poses = []
+        for i in range(self.fix_seq_length):
+            ecef_from_local = orient.rot_from_quat(frame_orientations[i])
+            local_from_ecef = ecef_from_local.T
+            frame_positions_local = np.einsum('ij,kj->ki', local_from_ecef, frame_positions - frame_positions[i]).astype(np.float32)
+            
+            future_poses.append(frame_positions_local[i: i+10])
+        future_poses = torch.tensor(future_poses)
+
+        return dict(
+            seq_input_img=input_img,  # torch.Size([N, 6, 128, 256])
+            seq_future_poses=future_poses,  # torch.Size([N, num_pts, 3])
+            # camera_intrinsic=torch.tensor(seq_samples[0]['camera_intrinsic']),
+            # camera_extrinsic=torch.tensor(seq_samples[0]['camera_extrinsic']),
+            # camera_translation_inv=torch.tensor(seq_samples[0]['camera_translation_inv']),
+            # camera_rotation_matrix_inv=torch.tensor(seq_samples[0]['camera_rotation_matrix_inv']),
+        )
+
+
 if __name__ == '__main__':
     from torch.utils.data import DataLoader
     from tqdm import tqdm
+    from utils import draw_trajectory_on_ax
+    import matplotlib.pyplot as plt
 
-    dataset = SequencePlanningDataset(split='val', json_path_pattern='p3_10pts_can_bus_%s_temporal.json')
+    dataset = Comma2k19SequenceDataset('data/comma2k19_val_non_overlap.txt', 'data/comma2k19/', use_memcache=False)
     for sample in dataset:
-        print(sample[0].shape, sample[1].shape)
+        for k, v in sample.items():
+            print(k, ':', v.shape, v.dtype)
+        for img, traj in zip(sample['seq_input_img'], sample['seq_future_poses']):
+            trajectories = list(traj.numpy().reshape(1, 10, 3))  # M, num_pts, 3
+            print(trajectories)
+
+            fig, ax = plt.subplots()
+            ax = draw_trajectory_on_ax(ax, trajectories, [1, ])
+            plt.tight_layout()
+            plt.show()
+
 
     exit()
 
